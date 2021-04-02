1324abf7aa692df5df99ce10274ef09207ee5f50
riederdominik <rieder.dominik@gmx.at>
2021-04-02 23:06:09 +0200

    refac: delete comments Obstacles.cd

--- Assets/MyGame/Scripts/Obstacle.cs
+++ Assets/MyGame/Scripts/Obstacle.cs
@@ -10,29 +10,22 @@ private void Awake()
     rb = GetComponent<Rigidbody2D>();
 }
 
-// Start is called before the first frame update
 void Start()
 {
         
 }
 
-// Update is called once per frame
 void Update()
 {
-    //if obstacle's position x is < -15f it will be destroyed
     if(transform.position.x < -15f)
     {
         Destroy(gameObject);
-}
-        //if obstacle's position x is < -15f it will be destroyed
-        if (transform.position.x > 15f)
-        {
-            Destroy(gameObject);
-        }
-
     }
-
-
+    if (transform.position.x > 15f)
+    {
+        Destroy(gameObject);
+        }
+}
 
 private void FixedUpdate()
 {
